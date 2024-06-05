292fa39fe59355081b47283011eca9935a2afcab
DaljotKaur <daljotkaur@edu.sait.ca>
2024-06-05 11:14:27 -0600

    week2

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,19 +1,11 @@
-// Import necessary dependencies
-import React from 'react';
-import Link from 'next/link';
-
-// Define the Page component
-const Page = () => {
+export default function Page() {
   return (
-    <div>
-      <h1> CPRG 306: Web Development 2 - Assignments</h1>
-      {/* Link to the week-2 page */}
-      <Link href="/week-2/student-info">
-        <a>Week 2 Assignment</a>
-      </Link>
-    </div>
+    <main>
+      <h1>Shopping List</h1>
+      <p>Daljot Kaur</p>
+      <a href="https://github.com/daljotkaur" target="_blank" rel="noopener noreferrer">
+        My GitHub
+      </a> 
+    </main>
   );
-};
-
-// Export the Page component as the default export
-export default Page;
+}
